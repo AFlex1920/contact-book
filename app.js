30c05c0518d54f5a055ffca5eb1bb8e841a454e1
AFlex1920 <anhnguyenflex1920@gmail.com>
2024-08-03 20:07:47 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const contactsRouter = require("./app/routes/contact.route");
-console.log(typeof contactsRouter);
+const ApiError = require("./app/api-error");
+
 const app = express();
 
 app.use(cors());
@@ -10,6 +11,22 @@ app.use("/api/contacts", contactsRouter);
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application." });
+});
+//handler 404 response
+app.use((req, res, next) => {
+  //Doan code nay se chay khi khong co route duoc dinh nghia nao khop voi
+  //yeu cau. Goi next() de chuyen sang middleware xu ly loi
+  return next(new ApiError(404, "Resource not found"));
+});
+
+//define error-handling middleware last, after other app.use() and routes calls
+app.use((err, req, res, next) => {
+  //Middleware xu ly loi tap trung
+  //Trong cac doan code xu ly o cac route, goi next(error) se chuyen ve middleware
+  //xu ly loi nay
+  return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+  });
 });
 
 module.exports = app;
